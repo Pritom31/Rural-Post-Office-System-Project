0ca7637c649a8d87d1ba3c80a51681b1ebf59f38
Pritom Basu <98013132+Pritom31@users.noreply.github.com>
2026-01-20 20:37:45 +0530

    Add files via upload

---
+++ com.rural.post.office/com.rural.post.office/src/main/java/com/rural/post/office/RuralPostOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.rural.post.office;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuralPostOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuralPostOfficeApplication.class, args);
+	}
+
+}
---
+++ com.rural.post.office/com.rural.post.office/src/test/java/com/rural/post/office/RuralPostOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rural.post.office;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuralPostOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
